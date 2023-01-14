8f32528483312890ccbde9553fc8c5f6206ebc8a
nhan <nhan0385790927@gmail.com>
2023-01-14 10:53:41 +0700

    init project

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from '../components/Header/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    </>
+  );
+}
+
+export default App;
